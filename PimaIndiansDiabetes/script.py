6eb276ffc396b9eb45c05b0c5152b3369a22e887
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2020-05-26 01:46:55 +0100

    fixes

--- PimaIndiansDiabetes/script.py
+++ PimaIndiansDiabetes/script.py
@@ -5,7 +5,6 @@ from tensorflow.keras.models import model_from_json
 from tensorflow.keras.models import model_from_yaml
 from tensorflow.keras.utils import plot_model
 import matplotlib.pyplot as plt
-import utils
 
 # fixar random seed para se puder reproduzir os resultados
 seed = 9
@@ -64,7 +63,7 @@ def compile_model(model):
 
 # Etapa 4 - treinar a rede (Fit the model) neste caso foi feito com os dados todos
 '''
-'batch_size'núermo da casos processados de cada vez
+'batch_size' número de casos processados de cada vez
 model.fit(X, Y, nb_epoch=150, batch_size=10, verbose=2)
 verbose: 0 para print do log de treino stdout, 1 para barra de progresso, 2 para uma
 linha por epoch.
@@ -79,8 +78,8 @@ def fit_model(model,input_attributes,output_attributes):
 #utils para visulaização do historial de aprendizagem
 def print_history_accuracy(history):
     print(history.history.keys())
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
@@ -99,9 +98,11 @@ def print_history_loss(history):
 
 # Etapa 5 - Calcular o desempenho do modelo treinado (neste caso utilizando os dados usados no treino)
 def model_evaluate(model,input_attributes,output_attributes):
-    print("###########inicio do evaluate###############################\n")
+    print("########### inicio do evaluate ###############################\n")
     scores = model.evaluate(input_attributes, output_attributes)
     print("\n metrica: %s: %.2f%%\n" % (model.metrics_names[1], scores[1]*100))
+    print("\n metrica: %s: %.2f%%\n" % (model.metrics_names[0], scores[0]*100))
+    
 
 # Etapa 6 - Utilizar o modelo treinado e escrever as previsões para novos casos
 def model_print_predictions(model,input_attributes,output_attributes):
@@ -117,7 +118,7 @@ def model_print_predictions(model,input_attributes,output_attributes):
 
 # Ciclo completo executando as Etapas 1,2,3,4,5 e 6
 def ciclo_completo():
-    (input_attributes,output_attributes) = read_cvs_dataset("pima-indiansdiabetes.csv", 8)
+    (input_attributes,output_attributes) = read_cvs_dataset("pima-indians-diabetes.csv", 8)
     model = create_model()
     print_model(model,"model_MLP.png")
     compile_model(model)
@@ -127,9 +128,9 @@ def ciclo_completo():
     model_evaluate(model,input_attributes,output_attributes)
     model_print_predictions(model,input_attributes,output_attributes)
 
-# exemplos de utilização deos utils
+# exemplos de utilização dos utils
 def ciclo_ler_dataset_treinar_gravar():
-    (input_attributes,output_attributes) = read_cvs_dataset("pima-indiansdiabetes.csv",8)
+    (input_attributes,output_attributes) = read_cvs_dataset("pima-indians-diabetes.csv",8)
     model = create_model()
     print_model(model,"model2.png")
     compile_model(model)
@@ -147,6 +148,62 @@ def ciclo_ler_modelo_evaluate_usar(input_attributes,output_attributes):
     compile_model(model)
     model_evaluate(model,input_attributes,output_attributes)
     model_print_predictions(model,input_attributes,output_attributes)
+
+
+# UTILS:
+# Utils para gravar modelos e pesos utilizá-los posteriormente
+'''
+Gravar um modelo num ficheiro utilizando o formato json. O nome do ficheiro deve ter a
+extensão .json
+'''
+def save_model_json(model,fich):
+    model_json = model.to_json()
+    with open(fich, "w") as json_file:
+        json_file.write(model_json)
+
+'''
+Gravar um modelo num ficheiro utilizando o formato yaml. O nome do ficheiro deve ter a
+extensão .yaml
+'''
+def save_model_yaml(model,fich):
+    model_yaml = model.to_yaml()
+    with open(fich, "w") as yaml_file:
+        yaml_file.write(model_yaml)
+
+'''
+Gravar os pesos de um modelo treinado num ficheiro utilizando o formato HDF5. O nome do
+ficheiro deve ter a extensão .h5
+'''
+def save_weights_hdf5(model,fich):
+    model.save_weights(fich)
+    print("Saved model to disk")
+
+'''
+Ler um modelo de um ficheiro no formato json e criar o respetivo modelo em memória.
+'''
+def load_model_json(fich):
+    json_file = open(fich, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    return loaded_model
+
+'''
+Ler um modelo de um ficheiro no formato yaml e criar o respetivo modelo em memória.
+'''
+def load_model_yaml(fich):
+    yaml_file = open(fich, 'r')
+    loaded_model_yaml = yaml_file.read()
+    yaml_file.close()
+    return model_from_yaml(loaded_model_yaml)
+
+'''
+Ler os pesos um modelo treinado de um ficheiro no formato hdf5 para o respectivo
+modelo.
+'''
+def load_weights_hdf5(model,fich):
+    model.load_weights(fich)
+    print("Loaded model from disk")
 
 # MAIN FUNCTION
 if __name__ == '__main__':
